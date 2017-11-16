ad8902c589de2afcc42ca10b8458fa2134f39f18
Alex Simonenko <simonenkoa@gmail.com>
2017-11-16 15:44:54 +0300

    ip address 192 168 x x PRINT

---
+++ print_IP_adress_192_168_x_x.py
@@ -0,0 +1,14 @@
+
+
+# while n < 100000 :
+#     print(n)
+#
+#     n += 1
+a = '192'
+b = '168'
+c = range(0, 256)
+d = range(0, 256)
+
+for y in c:
+    for x in d:
+        print(a, b, str(y), str(x), sep=".")
